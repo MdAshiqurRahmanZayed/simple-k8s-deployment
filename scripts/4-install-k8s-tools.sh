51b643e807c623af7cd0e001d4a3c589d7138164
MdAshiqurRahmanZayed <mdzayed2019@gmail.com>
2026-04-14 16:28:49 +0600

    Simplify Kubernetes tools installation by removing version specification

--- scripts/4-install-k8s-tools.sh
+++ scripts/4-install-k8s-tools.sh
@@ -3,12 +3,8 @@ set -e
 
 echo "=== Step 4: Install kubectl, kubeadm, kubelet ==="
 
-KUBERNETES_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt | cut -d '+' -f1)
-
-echo "Installing Kubernetes version: ${KUBERNETES_VERSION}"
-
 sudo apt-get update
-sudo apt-get install -y kubelet=${KUBERNETES_VERSION}-* kubeadm=${KUBERNETES_VERSION}-* kubectl=${KUBERNETES_VERSION}-*
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
 echo "=== Kubernetes tools installed successfully ==="
